6e4b139db31601432f89b5ed942fdac7d368e9e1
Kevin Wang <kevin_wang95@yahoo.com>
2016-11-01 17:22:45 +0800

    AIS Complete

--- run.m
+++ run.m
@@ -2,7 +2,10 @@ clear all;
 load h10.mat
 Z_theory = 0;
 
-load test.mat
-testbatchdata = reshape(permute(testbatchdata, [1,3,2]),size(testbatchdata,1) ...
-    *size(testbatchdata,2),size(testbatchdata,3));
+Z_10 = AIS(parameter_W, parameter_a, parameter_a, parameter_b, parameter_b, ...
+    zeros(size(parameter_W,1),1));
 
+% load test.mat
+% testbatchdata = reshape(permute(testbatchdata, [1,3,2]),size(testbatchdata,1) ...
+%     *size(testbatchdata,2),size(testbatchdata,3));
+% 
