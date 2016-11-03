eee48332ca303a241e846d5977710197bb237b2a
kevin Wang <kevin_wang95@yahoo.com>
2016-11-03 11:55:01 +0800

    AIS and TAP

--- AIS.m
+++ AIS.m
@@ -5,22 +5,37 @@ function Z = AIS(WB, aA, aB, bA, bB, v0)
 %   WA is zeros matrix
 
     WA = zeros(size(WB));
-    beta = sort(rand(1,20000));
+    beta = sort(rand(1,30000));
     beta(1) = 0; beta(end) = 1;
     v = v0;
     Z = 1;
     for i = 1:length(aA)
         Z = Z*(1+exp(aA(i)));
     end
-    PA = 1;
     for i = 1:length(bA)
         Z = Z*(1+exp(bA(i)));
-        PA = PA/(1+exp(-bA(i)));
     end
     
-    Z = log10(Z);
+    Z = log(Z);
     
-    for k = 1:length(beta)
+    for k = 1:length(beta)-1
+        Pk = exp((1-beta(k))*(bA*v));
+        for j = 1:length(aA)
+            Pk = Pk * (1+exp((1-beta(k))*(WA(:,j)'*v+aA(j))));
+        end
+        Pk = Pk * exp(beta(k)*(bB*v));
+        for j = 1:length(aB)
+            Pk = Pk * (1+exp(beta(k)*(WB(:,j)'*v+aB(j))));
+        end
+        Pk_1 = exp((1-beta(k+1))*(bA*v));
+        for j = 1:length(aA)
+            Pk_1 = Pk_1 * (1+exp((1-beta(k+1))*(WA(:,j)'*v+aA(j))));
+        end
+        Pk_1 = Pk_1 * exp(beta(k+1)*(bB*v));
+        for j = 1:length(aB)
+            Pk_1 = Pk_1 * (1+exp(beta(k+1)*(WB(:,j)'*v+aB(j))));
+        end
+        Z = Z + log(Pk_1/Pk);
         pA = zeros(1,size(WA,2));
         hA = zeros(1,size(WA,2));
         for j = 1:size(WA,2)
@@ -48,28 +63,7 @@ function Z = AIS(WB, aA, aB, bA, bB, v0)
             else
                 v(i) = 0;
             end
-        end
-        Pk = exp((1-beta(k))*(bA*v));
-        for j = 1:length(aA)
-            Pk = Pk * (1+exp((1-beta(k))*(WA(:,j)'*v+aA(j))));
-        end
-        Pk = Pk * exp(beta(k)*(bB*v));
-        for j = 1:length(aB)
-            Pk = Pk * (1+exp(beta(k)*(WB(:,j)'*v+aB(j))));
-        end
-        if(k == 1)
-            Z = Z + log10(Pk/PA);
-        else
-            Pk_1 = exp((1-beta(k-1))*(bA*v));
-            for j = 1:length(aA)
-                Pk_1 = Pk_1 * (1+exp((1-beta(k-1))*(WA(:,j)'*v+aA(j))));
-            end
-            Pk_1 = Pk_1 * exp(beta(k-1)*(bB*v));
-            for j = 1:length(aB)
-                Pk_1 = Pk_1 * (1+exp(beta(k-1)*(WB(:,j)'*v+aB(j))));
-            end
-            Z = Z + log10(Pk/Pk_1);
-        end
+        end        
     end
 end
 
---
+++ TAP.m
@@ -0,0 +1,38 @@
+function Z = TAP(W, a, b)
+%TAP 此处显示有关此函数的摘要
+%   a and b are row vectors
+    mh = zeros(1,size(W,2));
+    mv = zeros(1,size(W,1));
+    flag = true;
+    mh_pre = rand(1,size(W,2));
+    mv_pre = rand(1,size(W,1));
+    i = 0;
+    min_mh = inf;
+    min_mv = inf;
+    while flag
+        if abs(norm(mh-mh_pre))>1.2*min_mh || abs(norm(mv-mv_pre))>1.2*min_mv || i <50000 
+            mh_pre = mh;
+            mv_pre = mv;
+            mh = sigm(b + mv*W - (mv-mv.^2)*W.^2.*(mh-0.5));
+            mv = sigm(a + (W*mh')' - (W.^2*(mh-mh.^2)'.*(mv-0.5)')');
+        else
+            flag = false;
+        end
+        if i > 30000
+            if abs(norm(mh-mh_pre))< min_mh 
+                min_mh = abs(norm(mh-mh_pre));
+            end
+            if abs(norm(mv-mv_pre))< min_mv 
+                min_mv = abs(norm(mv-mv_pre));
+            end
+        end
+        i = i+1;
+    end
+    S = - (mh*log(mh') + (1-mh)*log((1-mh)') + mv*log(mv') + (1-mv)*log((1-mv)')); 
+    T = -S - a*mv' - b*mh' - mv*W*mh' - 0.5*(mv-mv.^2)*W.^2*(mh-mh.^2)';
+    Z = -T;
+end
+
+function s = sigm(x)
+    s = 1./(1+exp(-x));
+end
