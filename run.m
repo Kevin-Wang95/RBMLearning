cf7add36f2a6e9bfce3ccc1a482010fcf03248b3
Kevin Wang <kevin_wang95@yahoo.com>
2016-11-14 20:59:32 +0800

    Fin

--- run.m
+++ run.m
@@ -17,18 +17,18 @@ load h20.mat
 z(2) = AIS(parameter_W, parameter_a, parameter_a, parameter_b, parameter_b, ...
     zeros(size(parameter_W,1),1), 200000);
 z(6) = log(sum(exp(testbatchdata*parameter_b' + sum(log(1+exp(repmat(parameter_a,[size(testbatchdata,1),1])+ ...
-    testbatchdata*parameter_W)),2)-z(3))));
+    testbatchdata*parameter_W)),2)-z(2))));
 
 load h100.mat
 z(3) = AIS(parameter_W, parameter_a, parameter_a, parameter_b, parameter_b, ...
     zeros(size(parameter_W,1),1),60000);
 z(7)= log(sum(exp(testbatchdata*parameter_b' + sum(log(1+exp(repmat(parameter_a,[size(testbatchdata,1),1])+ ...
-    testbatchdata*parameter_W)),2)-z(5))));
+    testbatchdata*parameter_W)),2)-z(3))));
 
 load h500.mat
 z(4) = AIS(parameter_W, parameter_a, parameter_a, parameter_b, parameter_b, ...
     zeros(size(parameter_W,1),1),60000);
 z(8) = log(sum(exp(testbatchdata*parameter_b' + sum(log(1+exp(repmat(parameter_a,[size(testbatchdata,1),1])+ ...
-    testbatchdata*parameter_W)),2)-z(7))));
+    testbatchdata*parameter_W)),2)-z(4))));
 
 save z.mat z
